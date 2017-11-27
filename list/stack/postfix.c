b2cfbfbb2f4f64d11fda5169cdb772476a98ed2f
Silicon014 <kaguya.h@foxmail.com>
2017-11-27 16:34:56 +0800

    debugged

--- list/stack/postfix.c
+++ list/stack/postfix.c
@@ -2,10 +2,16 @@
 #define PRINT "%d\n"
 
 #include<stdio.h>
-#include<stdlib.h>
-#include"stack.h"
 #include<ctype.h>
-#include"postfix.h"
+#include"stack.h"
+
+Stack selector(Element, Stack);
+Stack put_together(Stack);
+Stack Addition(Stack);
+Stack Subtraction(Stack);
+Stack Multiply(Stack);
+Stack Division(Stack);
+Element ctoe(Element);
 
 typedef TYPE Element;
 int main(void)
@@ -56,13 +62,17 @@ Stack put_together(Stack s)
 	Element x1, x2;
 	
 	//读取
-	x1 = Top(s);
+	x1 = ctoe(Top(s));
 	s = Pop(s);
-	x2 = Top(s);
+	if ((x2 = Top(s)) == 0)
+	{
+		s = Push(x1, s);
+		return s;
+	}
 	s = Pop(s);
 
 	//计算并写入结果
-	s = Push(x1 * 10 + x2, s);
+	s = Push(x2 * 10 + x1, s);
 	
 	return s;
 }
@@ -126,4 +136,37 @@ Stack Division(Stack s)
 	s = Push(x2 / x1, s);
 
 	return s;
+}
+Element ctoe(Element i)
+{
+	char ch;
+
+	ch = (char)i;
+	switch (ch)
+	{
+		case '0':
+			return (Element)0;
+		case '1':
+			return 1;
+		case '2':
+			return 2;
+		case '3':
+			return 3;
+		case '4':
+			return 4;
+		case '5':
+			return 5;
+		case '6':
+			return 6;
+		case '7':
+			return 7;
+		case '8':
+			return 8;
+		case '9':
+			return 9;
+		default:
+			break;
+	}
+	fprintf(stderr, "ctoe error, default 0\n");
+	return 0;
 }
